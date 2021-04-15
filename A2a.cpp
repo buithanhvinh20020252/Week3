06d1f2f3c2467138c5b76759af69c853c103fe73
buithanhvinh20020252 <78576070+buithanhvinh20020252@users.noreply.github.com>
2021-04-15 10:29:56 +0700

    Add files via upload

---
+++ A2a.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string s;
+	int i;
+	int size = s.size();
+	for(i=0;i<size;i++)
+	{
+		cout << s[i];
+	}
+	cout << endl << s;
+	return 0;
+}
